652493240d94ed4ba9a60b0cee997530738084ac
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-02-21 16:08:05 +0500

    Revert "done with filteration"
    This reverts commit 4f4a58a9005c909f035012adbbfe7ba708107424.

--- index.php
+++ index.php
@@ -1276,9 +1276,9 @@ if (isset($_POST['drugName2'])) {
         } else { // Element found in the array
           hidden_compound.splice(index, 1); // Remove the element from the array
         }
+
         range_of_links(minValue, maxValue, slider_range);
-        
-        
+
       }
       //  handle the double click here 
 
@@ -1596,24 +1596,25 @@ if (isset($_POST['drugName2'])) {
       })
 
       // slider2.max = parentnodes.size();
-      let filternodes3 = [];
-
-      parentnodes.each(function(drugNode, i) {
+      let filternodes3 = parentnodes.each(function(drugNode, i) {
         if (i < valueofslider) {
 
 
           d3.select(this).style("display", null);
 
         } else {
-          console.log("drugnode", drugNode)
-          filternodes3.push(drugNode.id);
+          d3.select(this).style("display", "none");
           link.filter(function(linktemp) {
             if (linktemp.source === drugNode) {
               d3.select(this).style("display", "none")
             }
           })
         }
-      });
+
+});
+
+
+
 
 
       filteredLinks = link.filter(link => {
@@ -1791,8 +1792,6 @@ if (isset($_POST['drugName2'])) {
           return childNode;
         }
       });
-
-
       childnodefilteration.style("display", "none");
       let source_node = [];
       let matchinglinkpart = link.filter(function(link) {
@@ -1803,8 +1802,6 @@ if (isset($_POST['drugName2'])) {
           );
         }
       });
-
-
       matchinglinkpart.style("display", "none");
       node.each(function(d) {
         // d3.select(this).style("display", "none");   
@@ -1869,26 +1866,26 @@ if (isset($_POST['drugName2'])) {
         }
       })
 
-      // node.each(function(d) {
-      //   if (d.type === "parentnode") {
-      //     var nodestyle = d3.select(this).style("display");
-      //     const connectedLinks = link.filter(link => link.source.id === d.id);
-      //     // Array to store styles of connected links
-      //     var linkStyles = [];
+      node.each(function(d) {
+        if (d.type === "parentnode") {
+          var nodestyle = d3.select(this).style("display");
+          const connectedLinks = link.filter(link => link.source.id === d.id);
+          // Array to store styles of connected links
+          var linkStyles = [];
 
-      //     connectedLinks.each(function(link) {
-      //       var linkStyle = d3.select(this).style("display");
+          connectedLinks.each(function(link) {
+            var linkStyle = d3.select(this).style("display");
 
-      //       linkStyles.push(linkStyle);
-      //     });
-      //     // Check if every style in the array is "none"
-      //     var allLinksNone = linkStyles.every(style => style === "none");
-      //     if (allLinksNone && !hidden_compound.includes(d.id)) {
-      //       // Set node style to "display: none"
-      //       d3.select(this).style("display", "none");
-      //     }
-      //   }
-      // });
+            linkStyles.push(linkStyle);
+          });
+          // Check if every style in the array is "none"
+          var allLinksNone = linkStyles.every(style => style === "none");
+          if (allLinksNone && !hidden_compound.includes(d.id)) {
+            // Set node style to "display: none"
+            d3.select(this).style("display", "none");
+          }
+        }
+      });
 
       // link filter nodes here 
       link.filter(function(templink) {
@@ -1963,8 +1960,9 @@ if (isset($_POST['drugName2'])) {
 
       node.filter(function(node) {
         if (node.type === "parentnode" && hidden_compound.includes(node.id)) {
-          
+          console.log("check type")
           d3.select(this)
+
             .selectAll("circle") // Select all circles within this node
             .data([node]) // Bind data to the selection
             .enter() // Enter selection
@@ -1976,26 +1974,15 @@ if (isset($_POST['drugName2'])) {
             .attr("cy", -2)
             .style("stroke-dasharray", "5,5");; // Move 2 pixels up
 
-          return true; // Keep this node in the selection
-        } else if (node.type === "parentnode") {
-          d3.select(this)
-            .selectAll("circle")
-            .remove(); // Remove circle if it exists
+      //         return true; // Keep this node in the selection
+      //     } else if(node.type === "parentnode") {
+      //         d3.select(this)
+      //             .selectAll("circle")
+      //             .remove(); // Remove circle if it exists
 
-          return false; // Exclude this node from the selection
-        }
-      })
-
-      node.each(function(d) {
-        if (filternodes3.includes(d.id)) {
-          console.log("checkc", d3.select(this).style("display"))
-          d3.select(this).style("display", "none");
-
-          console.log("check", d3.select(this).style("display"))
-        }
-      });
-
-
+      //         return false; // Exclude this node from the selection
+      //     }
+      // })
       // ended 
       // count the number of the child
       let child_count = visible_childnode.length;
